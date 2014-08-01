1c6b556fa131d0f242b02707a242c93f7438517e
Victor Nystad <vnystad@gmail.com>
2014-08-01 14:45:30 +0200

    added jshint

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var traceur = require('traceur');
 var shell = require('gulp-shell');
+var jshint = require('gulp-jshint');
+
 
 gulp.task('connect', function() {
     connect.server({
@@ -20,7 +22,7 @@ gulp.task('reload', function() {
 })
 
 gulp.task('watch', function() {
-    gulp.watch(['www/js/*.js', '!www/js/build.js'], ['traceur']);
+    gulp.watch(['www/js/*.js', '!www/js/build.js'], ['jshint', 'traceur']);
     gulp.watch(['www/js/build.js'], ['reload']);
 })
 
@@ -29,4 +31,10 @@ gulp.task('default', ['connect', 'watch']);
 gulp.task('setup', function() {
     gulp.src('node_modules/traceur/bin/traceur-runtime.js').pipe(gulp.dest('www/lib'));
     gulp.src('node_modules/lodash/dist/lodash.underscore.js').pipe(gulp.dest('www/lib'));
+})
+
+gulp.task('jshint', function() {
+    gulp.src(['www/js/*.js', '!www/js/build.js'])
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('jshint-stylish'));
 })
